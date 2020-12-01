c78c5e6d088f5167daad6e2c74ece4a5c9236ebf
Ryan L. Boyd <ryanb@ryanb.cc>
2020-12-01 08:09:11 +0000

    initial commit

---
+++ ContextObj.cs
@@ -0,0 +1,17 @@
+﻿namespace ContextObj
+{
+    public class ContextObj
+    {
+        public string[] LeftContext { get; set; }
+        public string[] RightContext { get; set; }
+        public string[] MatchedWords { get; set; }
+
+        public ContextObj(string[] l, string[] r, string[] w)
+        {
+            LeftContext = l;
+            RightContext = r;
+            MatchedWords = w;
+        }
+
+    }
+}
